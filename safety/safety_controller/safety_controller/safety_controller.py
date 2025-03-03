16e676325676cdb7a2835abf1776f67c3d525f3f
Audrey Lee <audrey16@mit.edu>
2025-03-03 12:35:43 +0000

    Setting up safety controller

--- safety/safety_controller/safety_controller/safety_controller.py
+++ safety/safety_controller/safety_controller/safety_controller.py
@@ -3,41 +3,54 @@ import numpy as np
 import rclpy
 from rclpy.node import Node
 from ackermann_msgs.msg import AckermannDriveStamped
-from visualization_msgs.msg import Marker
 from rcl_interfaces.msg import SetParametersResult
 
-from wall_follower.visualization_tools import VisualizationTools
-
-
-class WallFollower(Node):
+class SafetyController(Node):
 
     def __init__(self):
-        super().__init__("wall_follower")
+        super().__init__("safety_controller")
         # Declare parameters to make them available for use
-        # DO NOT MODIFY THIS! 
         self.declare_parameter("scan_topic", "default")
         self.declare_parameter("drive_topic", "default")
-        self.declare_parameter("side", "default")
-        self.declare_parameter("velocity", "default")
-        self.declare_parameter("desired_distance", "default")
 
         # Fetch constants from the ROS parameter server
-        # DO NOT MODIFY THIS! This is necessary for the tests to be able to test varying parameters!
         self.SCAN_TOPIC = self.get_parameter('scan_topic').get_parameter_value().string_value
         self.DRIVE_TOPIC = self.get_parameter('drive_topic').get_parameter_value().string_value
-        self.SIDE = self.get_parameter('side').get_parameter_value().integer_value
-        self.VELOCITY = self.get_parameter('velocity').get_parameter_value().double_value
-        self.DESIRED_DISTANCE = self.get_parameter('desired_distance').get_parameter_value().double_value
 		
         # This activates the parameters_callback function so that the tests are able
         # to change the parameters during testing.
-        # DO NOT MODIFY THIS! 
-        self.add_on_set_parameters_callback(self.parameters_callback)
+        # self.add_on_set_parameters_callback(self.parameters_callback)
   
-        # TODO: Initialize your publishers and subscribers here
+        self.cmd_sub = self.create_subscription(
+            AckermannDriveStamped,
+            self.SCAN_TOPIC,
+            self.listener_callback,
+            10)
+        self.safety_pub = self.create_publisher(
+            AckermannDriveStamped, 
+            self.DRIVE_TOPIC,
+            10)
+        self.DT = 0.1 # in seconds
 
-        # TODO: Write your callback functions here    
-    
+    def listener_callback(self, msg):
+        drive_cmd = msg.drive
+        drive_ang = drive_cmd.steering_angle
+        drive_speed = drive_cmd.speed
+
+        predicted_dist = drive_speed * self.DT
+
+        drive_msg = AckermannDriveStamped()
+        drive_msg.header.stamp = self.get_clock().now().to_msg()
+        drive_msg.header.frame_id = "base_link"
+
+        drive_msg.drive.steering_angle = 0.0 # set everything else to 0
+        drive_msg.drive.steering_angle_velocity = 0.0 # set everything else to 0
+        drive_msg.drive.speed = 0.0 # STOP THE CAR
+        drive_msg.drive.acceleration = 0.0 # set everything else to 0
+        drive_msg.drive.jerk = 0.0 # set everything else to 0      
+
+        self.drive_pub.publish(drive_msg)
+
     def parameters_callback(self, params):
         """
         DO NOT MODIFY THIS CALLBACK FUNCTION!
@@ -45,24 +58,25 @@ class WallFollower(Node):
         This is used by the test cases to modify the parameters during testing. 
         It's called whenever a parameter is set via 'ros2 param set'.
         """
-        for param in params:
-            if param.name == 'side':
-                self.SIDE = param.value
-                self.get_logger().info(f"Updated side to {self.SIDE}")
-            elif param.name == 'velocity':
-                self.VELOCITY = param.value
-                self.get_logger().info(f"Updated velocity to {self.VELOCITY}")
-            elif param.name == 'desired_distance':
-                self.DESIRED_DISTANCE = param.value
-                self.get_logger().info(f"Updated desired_distance to {self.DESIRED_DISTANCE}")
-        return SetParametersResult(successful=True)
+        pass
+        # for param in params:
+        #     if param.name == 'side':
+        #         self.SIDE = param.value
+        #         self.get_logger().info(f"Updated side to {self.SIDE}")
+        #     elif param.name == 'velocity':
+        #         self.VELOCITY = param.value
+        #         self.get_logger().info(f"Updated velocity to {self.VELOCITY}")
+        #     elif param.name == 'desired_distance':
+        #         self.DESIRED_DISTANCE = param.value
+        #         self.get_logger().info(f"Updated desired_distance to {self.DESIRED_DISTANCE}")
+        # return SetParametersResult(successful=True)
 
 
 def main():
     rclpy.init()
-    wall_follower = WallFollower()
-    rclpy.spin(wall_follower)
-    wall_follower.destroy_node()
+    safety_controller = SafetyController()
+    rclpy.spin(safety_controller)
+    safety_controller.destroy_node()
     rclpy.shutdown()
 
 
