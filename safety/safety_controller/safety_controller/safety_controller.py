c8e6442a7478c5088a2549ab0a8af78912d081a0
Audrey Lee <audrey16@mit.edu>
2025-03-03 12:25:42 +0000

    Creating safety controller package + reorg

---
+++ safety/safety_controller/safety_controller/safety_controller.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import numpy as np
+import rclpy
+from rclpy.node import Node
+from ackermann_msgs.msg import AckermannDriveStamped
+from visualization_msgs.msg import Marker
+from rcl_interfaces.msg import SetParametersResult
+
+from wall_follower.visualization_tools import VisualizationTools
+
+
+class WallFollower(Node):
+
+    def __init__(self):
+        super().__init__("wall_follower")
+        # Declare parameters to make them available for use
+        # DO NOT MODIFY THIS! 
+        self.declare_parameter("scan_topic", "default")
+        self.declare_parameter("drive_topic", "default")
+        self.declare_parameter("side", "default")
+        self.declare_parameter("velocity", "default")
+        self.declare_parameter("desired_distance", "default")
+
+        # Fetch constants from the ROS parameter server
+        # DO NOT MODIFY THIS! This is necessary for the tests to be able to test varying parameters!
+        self.SCAN_TOPIC = self.get_parameter('scan_topic').get_parameter_value().string_value
+        self.DRIVE_TOPIC = self.get_parameter('drive_topic').get_parameter_value().string_value
+        self.SIDE = self.get_parameter('side').get_parameter_value().integer_value
+        self.VELOCITY = self.get_parameter('velocity').get_parameter_value().double_value
+        self.DESIRED_DISTANCE = self.get_parameter('desired_distance').get_parameter_value().double_value
+		
+        # This activates the parameters_callback function so that the tests are able
+        # to change the parameters during testing.
+        # DO NOT MODIFY THIS! 
+        self.add_on_set_parameters_callback(self.parameters_callback)
+  
+        # TODO: Initialize your publishers and subscribers here
+
+        # TODO: Write your callback functions here    
+    
+    def parameters_callback(self, params):
+        """
+        DO NOT MODIFY THIS CALLBACK FUNCTION!
+        
+        This is used by the test cases to modify the parameters during testing. 
+        It's called whenever a parameter is set via 'ros2 param set'.
+        """
+        for param in params:
+            if param.name == 'side':
+                self.SIDE = param.value
+                self.get_logger().info(f"Updated side to {self.SIDE}")
+            elif param.name == 'velocity':
+                self.VELOCITY = param.value
+                self.get_logger().info(f"Updated velocity to {self.VELOCITY}")
+            elif param.name == 'desired_distance':
+                self.DESIRED_DISTANCE = param.value
+                self.get_logger().info(f"Updated desired_distance to {self.DESIRED_DISTANCE}")
+        return SetParametersResult(successful=True)
+
+
+def main():
+    rclpy.init()
+    wall_follower = WallFollower()
+    rclpy.spin(wall_follower)
+    wall_follower.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+    
