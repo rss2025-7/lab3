5cb651a006461e3d39edca617d671295fe8a2f98
audrey16 <audrey16@mit.edu>
2025-03-08 17:36:29 -0500

    Basic error logging for experimental evaluation

--- wall_follower/wall_follower/wall_follower.py
+++ wall_follower/wall_follower/wall_follower.py
@@ -7,6 +7,7 @@ from ackermann_msgs.msg import AckermannDriveStamped
 from visualization_msgs.msg import Marker
 from rcl_interfaces.msg import SetParametersResult
 from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Float32
 
 from wall_follower.visualization_tools import VisualizationTools
 from .lidar_reading import compute_least_squares_line
@@ -49,6 +50,9 @@ class WallFollower(Node):
 
         self.drive_publisher_ = self.create_publisher(AckermannDriveStamped, self.DRIVE_TOPIC, 10)
         self.lidar_subscriber_ = self.create_subscription(LaserScan, self.SCAN_TOPIC, self.listener_callback, 10)
+
+        # FOR EVALUATION
+        self.err_publisher_ = self.create_publisher(Float32, "wall_follower_logs", 10)
 
         self.line_pub = self.create_publisher(Marker, self.WALL_TOPIC, 1)
 
@@ -132,6 +136,10 @@ class WallFollower(Node):
         # self.get_logger().info(
         #     f"Distance: {distance:.2f}, Error: {error:.2f}, Steering: {steering_angle:.2f}"
         # )
+
+        msg = Float32()
+        msg.data = error
+        self.err_publisher_.publish(msg)
 
         # TODO: Write your callback functions here
 
